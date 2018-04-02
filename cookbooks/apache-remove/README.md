28a3972edfbbe9e6d310c2c1c278df9fb5bed61f
Ramakrishna Subbarao <ram5019@gmail.com>
2018-04-02 13:09:02 -0400

    added new cookbook to remove package modified the author

---
+++ cookbooks/apache-remove/README.md
@@ -0,0 +1,4 @@
+# apache-remove
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/apache-remove/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: apache-remove
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :remove
+end
