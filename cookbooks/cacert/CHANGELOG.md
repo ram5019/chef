fd80236c86b0d64e8af69880cad45f4f5f9e883c
Ramakrishna Subbarao <ram5019@gmail.com>
2018-04-03 08:41:13 -0400

    Import cacert version 0.5.0

---
+++ cookbooks/cacert/CHANGELOG.md
@@ -0,0 +1,34 @@
+# CHANGELOG for cacert
+
+This file is used to list changes made in each version of cacert.
+
+## 0.5.0:
+
+* Fix errors to ensure compatibility with Chef-13
+
+## 0.4.0:
+
+* Rename the `hash` attribute to `cert_hash` (used by the `cacert` (default)
+  and `cacert_hash` provider).
+
+  This change is a workaround, as Chef-12.16.42 apparently fails with a hard to
+  track down error message if an attribute is named `hash`. See this [Github
+  issue](https://github.com/chef/chef/issues/5565) for details.
+
+## 0.3.1:
+
+* Add support for checksums
+
+## 0.3.0:
+
+* Check whether hashes match when it's provided
+* Do not use https when fetching cacert.org certificates
+* cacert.org recipe now also installs cacert.org class 3 PKI
+
+## 0.2.0:
+
+* Add default recipe, that installs certificates according to attributes
+
+## 0.1.0:
+
+* Initial release of cacert
---
+++ cookbooks/cacert/providers/hash.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: cacert
+# Provider:: hash
+#
+# Copyright 2012, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use_inline_resources
+
+action :create do
+  # Generate hash using openssl
+  cmd = Mixlib::ShellOut.new("openssl x509 -hash -noout -in #{new_resource.cert_dir}/#{new_resource.cert}")
+  cmd.run_command
+  computed_hash = cmd.stdout.chomp if cmd.exitstatus
+
+  # If hash was provied, check whether the computed hash and the expected hash match
+  if new_resource.cert_hash && new_resource.cert_hash != computed_hash
+    Chef::Log.fatal("Hash #{computed_hash} of #{new_resource.cert} doesn't match expected hash #{new_resource.cert_hash}")
+  end
+
+  r = link new_resource.cert do
+    target_file "#{new_resource.cert_dir}/#{computed_hash}.0"
+    to ::File.basename(new_resource.cert)
+  end
+
+  new_resource.updated_by_last_action(true) if r.updated_by_last_action?
+end
