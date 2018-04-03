980335f834fa226fdd5117ff04f35cb79bf2b44f
Ramakrishna Subbarao <ram5019@gmail.com>
2018-04-03 08:35:01 -0400

    Import fb_swap version 0.1.0

---
+++ cookbooks/fb_swap/metadata.rb
@@ -0,0 +1,15 @@
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+name 'fb_swap'
+maintainer 'Facebook'
+maintainer_email 'noreply@facebook.com'
+license 'BSD-3-Clause'
+description 'Controls swap enable/disable'
+source_url 'https://github.com/facebook/chef-cookbooks/'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
+depends 'fb_dracut'
+depends 'fb_helpers'
+depends 'fb_systemd'
